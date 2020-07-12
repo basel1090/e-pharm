1d956c10f50ce0670bcbf7844d83f814f59ab09e
bqeshta <bqeshta@visionplus.ps>
2020-07-12 13:03:04 +0300

    Fix API

--- app/User.php
+++ app/User.php
@@ -5,10 +5,17 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
+use Laravel\Passport\HasApiTokens;
+
+
 
 class User extends Authenticatable
 {
+	
     use Notifiable;
+	use HasRoles;
+    use HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
